9b5d9b15860df56f4a9bbc335901b9f61dee592d
andredelgado <andrevieiradelgado@gmail.com>
2014-03-25 19:02:22 +0000

    wordpress functions

--- index.php
+++ index.php
@@ -10,23 +10,32 @@
 
   <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="screen" />
   <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/custom.js"></script>
-  </head>
+</head>
 <body>
   <header class="featured">
     <img src="http://themes.playnethemes.com/mayde/wp-content/uploads/2014/03/testcase31-copy.jpg">
   </header>
+  <div class="searchBox">
+  <input type="text" value="" name="s" id="s">
+  <img src="http://connormulcahey.com/searchIcon.svg" alt="" />
+</div>
   <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-  <article class="container articleS">
-    <div class="articleTitle">
-    <h1><?php the_title(); ?></h1>
+  <div class="articleS"></div>
+  <div class="articleBG">
+    <article class="container">
+      <div class="articleTitle">
+        <h1><?php the_title(); ?></h1>
+      </div>
+      <div class="articleShort">
+        <p>Interesting publisher names kept by developers in Windows Phone Store</p>
+      </div>
+      <span class="articleCat">
+      In: <?php the_category($name) ?>
+    </span>
+      <a href="<?php the_permalink(); ?>"><div class="btn right"><p>Read More</p></div></a>
+    </article>
   </div>
-  <div class="articleShort">
-    <p>Lorem Ipsum Dolor Ahmet</p>
-  </div>
-    <p>Categories</p>
-    <a href="<?php the_permalink(); ?>"><div class="btn right"><p>Read More</p></div></a>
-  </article>
-<?php endwhile; endif; ?>
+  <?php endwhile; endif; ?>
   <footer>
   </footer>
 </body>

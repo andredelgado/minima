3d773e1671001c117a037a6ff533eec7cb775ebf
andredelgado <andrevieiradelgado@gmail.com>
2014-03-27 19:45:13 +0000

    adjusting footer

--- footer.php
+++ footer.php
@@ -1,16 +1,23 @@
 	<footer>
-		<div class="container">
-			<div class="footerMenu">
-				<?php wp_nav_menu( array( 'container_class' =--> 'main-nav', 'theme_location' => 'primary' ) ); ?>
+		<div class="footerWrapper">
+			<div class="container">
+				<div class="widgetArea">
+					<?php if ( is_active_sidebar( 'footer-widget-area' ) ) { ?>
+  						<aside class="widgets" role="complementary">
+    						<?php dynamic_sidebar( 'footer-widget-area' ); ?>
+  						</aside>
+					<?php } ?>
+				</div>
 			</div>
-			<div class="widgetArea">
-				<?php if ( is_active_sidebar( 'footer-widget-area' ) ) { ?>
-  					<aside class="widgets" role="complementary">
-    			<?php dynamic_sidebar( 'footer-widget-area' ); ?>
-  					</aside>
-				<?php } ?>
-			</div>
-			<div class="footerCredits">
+		</div>
+		<div class="footerBar">
+			<div class="container">
+				<div class="footerCredits left">
+					<a href="<?php echo home_url(); ?>"> <?php bloginfo('name'); ?> </a> powerd by <a href="http://andreny.com/minima">minima.</a>
+				</div>
+				<div class="footerMenu right">
+					<?php wp_nav_menu( array( 'container_class' => 'main-nav', 'theme_location' => 'primary' ) ); ?>
+				</div>
 			</div>
 		</div>
 	</footer>

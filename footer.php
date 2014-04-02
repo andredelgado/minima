f4f1031a332872f63c222600963caca3a7cd1138
andredelgado <andrevieiradelgado@gmail.com>
2014-04-02 06:01:09 +0100

    fix paddings on footer

--- footer.php
+++ footer.php
@@ -6,14 +6,14 @@
 						<div class="social">
 							<p><i class="fa fa-facebook"></i><a href="https://www.facebook.com/delgadoandre">@delgadoandre</a></p>
 							<p><i class="fa fa-instagram"></i><a href="http://www.instagram.com/andrevdelgado">@andrevdelgado</a></p>
-							<p><i class="fa fa-twitter"></i>@delgadoandre</p>
-							<p><i class="fa fa-github"></i>@andredelgado</p>
+							<p><i class="fa fa-twitter"></i><a href="http://twitter.com/delgadoandre">@delgadoandre</a></p>
+							<p><i class="fa fa-github"></i><a href="http://github.com/delgadoandre">@andredelgado</a></p>
 						</div>
 					</div>
-					<div class="widget" style="padding-left: 25px;">
+					<div class="widget" style="padding-left: 60px;">
 						 <?php wp_list_categories(); ?> 
 					</div>
-					<div class="widget" style="padding-left: 25px;">
+					<div class="widget" style="padding-left: 60px;">
 						<h2>Last Tweets</h2>
 						<div id="lastTweets"></div>
 					</div>

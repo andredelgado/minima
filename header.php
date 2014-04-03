74d7f98e998cd3fb958046137d026cb1da3359c6
andredelgado <andrevieiradelgado@gmail.com>
2014-04-03 16:36:21 +0100

    add reset.css

--- header.php
+++ header.php
@@ -8,6 +8,7 @@
   <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
   <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
 
+  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>css/reset.css" type="text/css" />
   <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="screen" />
   <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>fonts/font-awesome-4.0.3/css/font-awesome.min.css" type="text/css" />
 </head>
